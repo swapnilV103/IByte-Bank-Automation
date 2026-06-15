1266b62577b934017620055215656c54ddc73f3e
Swapnil Vidhate <swapnilvidhate@Swapnils-MacBook-Pro.local>
2026-06-15 23:44:34 +0530

    Swapnil | added hooks

---
+++ support/hooks.ts
@@ -0,0 +1,17 @@
+import {Before , After} from "@cucumber/cucumber";
+import {chromium} from "@playwright/test";
+import {MyWorld} from "./world.js";
+
+Before(async function (this:MyWorld) {
+    this.browser = await chromium.launch({
+        headless:false
+    })
+
+    this.context = await this.browser.newContext();
+    this.page = await this.context.newPage();
+  
+});
+
+After(async function (this:MyWorld){
+    await this.browser.close();
+})
